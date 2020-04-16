750e84fd816a04b39f7f1b6fa84e71486dd64087
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-16 12:05:02 -0400

    Allow Jenkins outside

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "aws_security_group" "default" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16", "0.0.0.0/0"]
   }
 
   # outbound internet access
