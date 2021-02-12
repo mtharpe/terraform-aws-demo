74255d37c0095d7d4bdfc67b7cea75505b1d15c6
Michael Tharpe <mtharpe@hashicorp.com>
2021-02-12 10:53:05 -0500

    Update to policy

--- main.tf
+++ main.tf
@@ -38,7 +38,8 @@ resource "aws_iam_policy" "policy" {
   "Statement": [
     {
       "Action": [
-        "ec2:Describe*"
+        "ec2:Describe*",
+        "iam:Create*"
       ],
       "Effect": "Allow",
       "Resource": "*"
