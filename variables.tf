83a336e809331c875ff1dcc5b7e25e827c495be2
Michael Tharpe <mtharpe@hashicorp.com>
2023-10-18 00:25:51 -0400

    Updated vars

--- variables.tf
+++ variables.tf
@@ -16,3 +16,14 @@ variable "key_name" {
   description = "Desired name of AWS key pair"
   default     = "mtharpe-demo-terraform"
 }
+
+variable "aws_instance_username" {
+  description = "Username for the AWS instance"
+  default = ""
+}
+
+variable "aws_instance_password" {
+  description = "Password for the AWS instance"
+  default   = ""
+  sensitive = true
+}
