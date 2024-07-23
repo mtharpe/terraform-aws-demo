5140284885d0fca65ae085e9441f04f0549a3e3c
Michael Tharpe <mtharpe@gmail.com>
2024-07-22 20:07:35 -0400

    Updated for lint

--- .tflint.hcl
+++ .tflint.hcl
@@ -1,3 +1,7 @@
 rule "terraform_required_version" {
   enabled = false
 }
+
+rule "terraform_unused_declarations" {
+  enabled = false
+}
--- variables.tf
+++ variables.tf
@@ -1,28 +1,34 @@
 variable "user" {
   description = "This is going to be the Org username"
+  type        = string
 }
 
 variable "public_key" {
   description = "Public key info"
+  type        = string
   sensitive   = true
 }
 
 variable "private_key" {
   description = "Private key info"
+  type        = string
   sensitive   = true
 }
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
+  type        = string
   default     = "mtharpe-demo-terraform"
 }
 
 variable "aws_instance_username" {
+  type        = string
   default = ""
 }
 
 variable "aws_instance_password" {
   default   = ""
+  type        = string
   sensitive = true
 }
 
